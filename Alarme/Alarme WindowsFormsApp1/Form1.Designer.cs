fd83b2f2f43775c1ec39a3f40cd8307aeb1c992f
Fábio Junik <fabiojunik@gmail.com>
2022-06-07 21:04:59 +0100

    App Alarme

---
+++ Alarme/Alarme WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Alarme_WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.bn_iniciar = new System.Windows.Forms.Button();
+            this.btn_parar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(54, 28);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(121, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.Value = new System.DateTime(2021, 5, 18, 17, 46, 0, 0);
+            // 
+            // bn_iniciar
+            // 
+            this.bn_iniciar.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.bn_iniciar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bn_iniciar.Location = new System.Drawing.Point(12, 113);
+            this.bn_iniciar.Name = "bn_iniciar";
+            this.bn_iniciar.Size = new System.Drawing.Size(75, 41);
+            this.bn_iniciar.TabIndex = 1;
+            this.bn_iniciar.Text = "Iniciar";
+            this.bn_iniciar.UseVisualStyleBackColor = false;
+            this.bn_iniciar.Click += new System.EventHandler(this.bn_iniciar_Click);
+            // 
+            // btn_parar
+            // 
+            this.btn_parar.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btn_parar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_parar.Location = new System.Drawing.Point(141, 113);
+            this.btn_parar.Name = "btn_parar";
+            this.btn_parar.Size = new System.Drawing.Size(75, 41);
+            this.btn_parar.TabIndex = 2;
+            this.btn_parar.Text = "Parar";
+            this.btn_parar.UseVisualStyleBackColor = false;
+            this.btn_parar.Click += new System.EventHandler(this.btn_parar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.AliceBlue;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(85, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Estado";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Goldenrod;
+            this.BackgroundImage = global::Alarme_WindowsFormsApp1.Properties.Resources.alarme1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(237, 206);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_parar);
+            this.Controls.Add(this.bn_iniciar);
+            this.Controls.Add(this.dateTimePicker1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Alarme";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button bn_iniciar;
+        private System.Windows.Forms.Button btn_parar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Alarme/Alarme WindowsFormsApp1/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace Alarme_WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        System.Timers.Timer tempo;
+        SoundPlayer tocar = new SoundPlayer();
+
+        delegate void UpdateLable(Label lb1, string value);
+
+        void UpDateLable(Label lb1,string value)
+        {
+            lb1.Text = value; 
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tempo = new System.Timers.Timer();
+            tempo.Interval = 1000;
+            tempo.Elapsed += Timer_Elapsed;
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            DateTime tempoCorreto =  DateTime.Now;
+            DateTime tempoUsuario = dateTimePicker1.Value;
+
+            if(tempoCorreto.Hour== tempoUsuario.Hour && tempoCorreto.Minute== tempoUsuario.Minute && tempoCorreto.Second == tempoUsuario.Second)
+            {
+                tempo.Stop();
+                try
+                {
+                    UpdateLable upd = UpDateLable;
+
+                    if (label1.InvokeRequired)
+                        Invoke(upd, label1, "Esta na hora");
+                    
+                    
+                    tocar.SoundLocation = @"C:\Windows\Media\Alarm05.wav";
+                    tocar.PlayLooping();
+                }catch(Exception ex)
+                {
+
+                }
+            }
+        }
+
+        private void bn_iniciar_Click(object sender, EventArgs e)
+        {
+            tempo.Start();
+            label1.Text = "Rodando...";
+        }
+
+        private void btn_parar_Click(object sender, EventArgs e)
+        {
+            tempo.Stop();
+            label1.Text = "Parado!";
+            tocar.Stop();   
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
